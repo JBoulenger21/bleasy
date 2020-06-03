f9126ad528ca29e054c91968adf8739e89af25be
JBoulenger21 <boulenger.julie@gmail.com>
2020-06-03 13:47:49 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    <link rel="icon" type="image/png" href="ressource/favicon.png">
+    <title>Blueasy</title>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,351 @@
+<?php
+include('header.php');
+?>
+
+<header>
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-between pt-5 pb-5 fixed-top">
+  <!-- Navbar -->
+        <!--logo-->
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="ressource/blueasy-logo.png" alt="logoblueasy"></a>
+            <!-- Links -->
+            <ul class="navbar-nav float-lg-right">
+              <li class="nav-item">
+                <a class="nav-link" href="#">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">SERVICES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">FEATURES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">PORTFOLIO</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">CONTACT</a>
+              </li>
+            </ul>
+        </div>
+    </nav>
+  </div>
+    <div class="container">
+      <div class=" header-text container text-justify text-center align-center p-auto">
+          <h1>"I'M LOOKING FOR THE UNEXPECTED.</h1>
+          <h1>I'M LOOKING FOR THINGS I'VE NEVER SEEN BEFORE."</h1>
+      </div>
+  </div>
+</header>
+
+<main>
+  <section id="services">
+    <div class="container-fluid bg-primary pt-5 pb-5">
+      <div class="container mt-3 mb-3">
+        <div class="ligne-deco">
+          <h2>SERVICES</h2>
+        </div>
+          <div class="row align-items-end justify-content-between mt-5 mb-5">
+              <div class="col-lg-3 col-sm-12 justify-content-between">
+                  <img class="m-3" src="ressource/blueasy-coffee.png" alt="coffee">
+                    <h3>COFFEE</h3>
+                    <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+              </div>
+              <div class="col-lg-3 col-sm-12">
+                  <img class="m-3" src="ressource/blueasy-instant.png" alt="instant">
+                    <h3>INSTANT</h3>
+                    <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+              </div>
+              <div class="col-lg-3 col-sm-12">
+                  <img class="m-3" src="ressource/blueasy-serious.png" alt="Serious">
+                    <h3>SERIOUS</h3>
+                    <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+              </div>
+              <div class="col-lg-3 col-sm-12">
+                  <img class="m-3" src="ressource/blueasy-frame" alt="Frame">
+                    <h3>FRAME</h3>
+                    <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  </section>
+  <section id="portfolio">
+    <div class="container-fluid pb-5"> <!--Portfolio-->
+        <div class="container"> <!--portfolio image clic-->
+          <nav class="navbar navbar-expand-sm bg-light navbar-light justify-content-between mt-5 mb-5">
+        <!-- menu portfolio -->
+              <!--logo-->
+              <a class="navbar-brand" href="#"><div class="ligne-deco-grey"><h2>PORTFOLIO</h2></div></a>
+              <!-- Links -->
+              <ul class="navbar-nav float-lg-right">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ALL</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">PANORAMAS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">PORTRAITS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">MACRO</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">JOURNAL</a>
+                </li>
+              </ul>
+          </nav>
+          <div class="row justify-content-around justify-content-between mt-3 mb-3">
+            <div class="col-lg-3 col-sm-12 mr-auto d-block">
+              <a href="#">
+                <div class="img-portfolio">
+                  <img class="img-fluid" src="ressource/blueasy-portfolio-1.jpg" alt="portfolio1">
+                      <div class="hover-portfolio">
+                        <p class="hover-text">+</p>
+                      </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-lg-3 col-sm-12 mr-auto d-block">
+              <a href="#">
+                <div class="img-portfolio">
+                  <img class="img-fluid" src="ressource/blueasy-portfolio-2.jpg" alt="portfolio2">
+                      <div class="hover-portfolio">
+                        <p class="hover-text">+</p>
+                      </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-lg-3 col-sm-12 mr-auto d-block">
+              <a href="#">
+                <div class="img-portfolio">
+                  <img class="img-fluid" src="ressource/blueasy-portfolio-3.jpg" alt="portfolio3">
+                      <div class="hover-portfolio">
+                        <p class="hover-text">+</p>
+                      </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-lg-3 col-sm-12 mr-auto d-block">
+              <a href="#">
+                <div class="img-portfolio">
+                  <img class="img-fluid" src="ressource/blueasy-portfolio-4.jpg" alt="portfolio4">
+                      <div class="hover-portfolio">
+                        <p class="hover-text">+</p>
+                      </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="row justify-content-around justify-content-between mt-3 mb-3">
+            <div class="col-lg-3 col-sm-12 mr-auto d-block">
+              <a href="#">
+                <div class="img-portfolio">
+                  <img class="img-fluid" src="ressource/blueasy-portfolio-5.jpg" alt="portfolio5">
+                      <div class="hover-portfolio">
+                        <p class="hover-text">+</p>
+                      </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-lg-3 col-sm-12 mr-auto d-block">
+              <a href="#">
+                <div class="img-portfolio">
+                  <img class="img-fluid" src="ressource/blueasy-portfolio-6.jpg" alt="portfolio6">
+                      <div class="hover-portfolio">
+                        <p class="hover-text">+</p>
+                      </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-lg-3 col-sm-12 mr-auto d-block">
+              <a href="#">
+                <div class="img-portfolio">
+                  <img class="img-fluid" src="ressource/blueasy-portfolio-7.jpg" alt="portfolio7">
+                      <div class="hover-portfolio">
+                        <p class="hover-text">+</p>
+                      </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-lg-3 col-sm-12 mr-auto d-block">
+              <a href="#">
+                <div class="img-portfolio">
+                  <img class="img-fluid" src="ressource/blueasy-portfolio-8.jpg" alt="portfolio8">
+                      <div class="hover-portfolio">
+                        <p class="hover-text">+</p>
+                      </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+    </div>
+  </section>
+  <section id="features">
+      <div class="container-fluid bg-dark"> <!-- Just default section -->
+          <div class="container pt-5">
+              <div class="row">
+                <div class="col-lg-6 col-sm-12">
+                  <div class="ligne-deco">
+                  <H2>JUST DEFAULT SECTION<h2>
+                  </div>
+                  <p>Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.
+
+                    Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+                    <button type="button" class="btn btn-outline-dark">VISIT ME</button>
+                </div>
+                <div class="col-lg-6 col-sm-12 embed-responsive mx-auto mb-5">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/IGN3u8TBO4w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="container-fluid bg-primary height-310px p-5"> <!-- Twitter -->
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="ligne-deco">
+                <H2>TWITTER<h2>
+                </div>
+              </div>
+              <div class="col-lg-6 d-flex justify-content-end">
+                <img class="object-contain" src="ressource/blueasy-logo-twt.png" alt="logo twitter">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <p class="text-twitter">Unerdwear cookie liquorice. Cake donut cupcake lollipop soufflé candy. Chocolate oat cake <span>@cheesecake</span> tootsie roll.<p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container-fluid who-is-bg p-5"> <!-- who is John doe -->
+            <div class="container">
+              <div class="ligne-deco">
+              <H2>WHO IS JOHN DOE?<h2>
+              </div>
+              <div class="row">
+                <div class="col-lg-6 col-sm-12">
+                  <p>Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.
+
+                    Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                  <p>Pudding dessert jujubes tiramisu gingerbread croissant tiramisu applicake. Sesame snaps sugar plum cotton candy chocolate bonbon lemon drops candy canes cotton candy. Cake toffee pie bear claw pastry.</p>
+                  <img src="ressource/blueasy-logo-carre-tw.png" alt="Logo Twitter Who is john doe">
+                  <img src="ressource/blueasy-logo-carre-gplus.png" alt="Logo google+ Who is john doe">
+                  <img src="ressource/blueasy-logo-carre-dribble.png" alt="Logo dribble Who is john doe">
+                  <img src="ressource/blueasy-logo-carre-coffee.png" alt="Logo coffee Who is john doe">
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="container"> <!-- Testimonials -->
+          <div class="row pt-5 pb-5">
+            <div class="col-6  text-testi">
+                <div class="ligne-deco-grey">
+                <H2 class=" text-testi">TESTIMONIALS<h2>
+                </div>
+            </div>
+            <div class="col-lg-6 d-flex justify-content-end">
+              <img class="object-contain" src="ressource/blueasy-logo-guillemet.png" alt="icone quote">
+            </div>
+          <div class="row">
+            <div class="col-lg-12  pr-5 pl-4">
+              <p class=" text-testi">Applicake chocolate cake wafer toffee pie soufflé wafer. Tart marshmallow wafer macaroon cheesecake jelly. Gingerbread cookie soufflé sweet roll sweet roll jelly-o.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </section>
+  <section id="contact"> <!--contact-->
+    <div class="container-fluid bg-dark-1 p-5">
+          <div class="container"> <!--formulaire-->
+            <div class="ligne-deco">
+              <H2>CONTACT<h2>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-6 col-sm-12 p-3">
+                <form action="/action_page.php" class="was-validated">
+                    <div class="form-group">
+                      <label for="uname">Full name:</label>
+                      <input type="text" class="form-control" id="uname" placeholder="" name="uname" required>
+                      <div class="valid-feedback">Valid.</div>
+                      <div class="invalid-feedback">Please fill out this field.</div>
+                    </div>
+                    <div class="form-group">
+                      <label for="Email">Email:</label>
+                      <input type="email" class="form-control" id="email" placeholder="" name="pswd" required>
+                      <div class="valid-feedback">Valid.</div>
+                      <div class="invalid-feedback">Please fill out this field.</div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 col-sm-12">
+                    <p></br></br>Jujubes bear claw croissant wafer sweet lemon drops brownie chocolate bar. Sweet lollipop halvah tootsie roll cake fruitcake. Chocolate cake chocolate bar gingerbread lollipop tart. Jujubes bear claw donut chupa chups.</p>
+                    <p>Linda Newman</br>
+                      W325 State Road 123</br>
+                      Mondovi, WI(Wisconsin) 98746-54321</br>
+                      (715) 946 - 1234<p>
+                  </div>
+                    <div class="col-lg-12">
+                    <div class="form-group">
+                      <label for="textarea">Message:</label>
+                      <input type="email" class="form-control form-control-2" id="email" placeholder="" name="textarea" required>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-sm-12">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">MESSAGE</button> <!--button message-->
+                  </div>
+                  </form>
+          </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+<footer>
+  <div class="container-fluid p-5 bg-dark-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+            <nav class="navbar navbar-expand-sm navbar-dark">
+              <ul class="nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><div class="text-muted">HOME</div></a>
+                </li>
+                <li>
+                  <p class="nav-link-2">|</p>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><div class="text-muted">SERVICES</div></a>
+                </li>
+                <li>
+                  <p class="nav-link-2">|</p>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><div class="text-muted">PORTFOLIO</div></a>
+                </li>
+                <li>
+                  <p class="nav-link-2">|</p>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><div class="text-muted">CONTACT</div></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div class="col-lg-6 d-flex justify-content-end">
+            <p class="text-muted">2013 ©</p>
+          </div>
+        </div>
+    </div>
+  </div>
+</footer>
+
+<?php
+include('footer.php');
+?>
